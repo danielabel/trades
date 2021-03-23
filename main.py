4cff63616612337c2f64865f083dc2558082f772
Dan Abel <dan.abel@tes.com>
2021-03-23 23:05:10 +0000

    basic exception handling

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from EpisodeApp import EpisodeApp
 @click.option('--file', default='./sample_data/python_exercise_trades.csv', help='CSV file to load')
 @click.option('--plot', default='plot.pdf', help='file to save plot of episode to')
 def click(file, plot):
-    app = EpisodeApp(file)
-    app.plot_episode(plot)
-    print(app.balance)
+    try:
+        app = EpisodeApp(file)
+        app.plot_episode(plot)
+        print(app.balance)
+    except KeyError as e:
+        print("Data file format not compatible - is there a column missing: ", str(e))
+    except Exception as e:
+        print("failed to process data. Error:", str(e))
 
 
 if __name__ == '__main__':
